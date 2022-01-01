41695a0621d55601e49328397ad54f6c7887ca4f
TheNam74 <nguyenthenam1910@gmail.com>
2022-01-01 14:48:32 +0700

    create log folder, wirte log file, read log file

--- ChildrenSideApp/MainWindow.xaml.cs
+++ ChildrenSideApp/MainWindow.xaml.cs
@@ -18,37 +18,54 @@ using System.Windows.Shapes;
 
 namespace ChildrenSideApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        string FILEPATH = "C:\\Users\\PC\\VNU-HCMUS\\VŨ CÔNG DUY - he dieu hanh\\thuc muc sync\\test.txt";
+        string ROOTPATH = "C:\\Users\\PC\\VNU-HCMUS\\VŨ CÔNG DUY - he dieu hanh\\thuc muc sync\\";
+        string SCHEDULEFILEPATH = "schedule.txt";
+        string LOGFOLDERPATH, LOGFILEPATH;
         public MainWindow()
         {
             InitializeComponent();
-            //init
+            //init file path
+            string dateStr = DateTime.Now.ToString("dd-MM-yyyy");
+            LOGFOLDERPATH = dateStr + "\\";
+            SCHEDULEFILEPATH = ROOTPATH + SCHEDULEFILEPATH;
+            LOGFOLDERPATH = ROOTPATH + LOGFOLDERPATH;
+            LOGFILEPATH = LOGFOLDERPATH + "log.txt";
+
         }
 
         //log in
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //test CTime class
-            //CTime schedule = new CTime(1, 2, 3, 4, 5, 6, 7);
-            //schedule.From.Hour =2;
-            //schedule.From.Minute =2;
-            //schedule.To.Hour =2;
-            //schedule.To.Minute =2;
-            //schedule.Duration = 2;
-            //schedule.Duration =2;
-            //schedule.Interupt = 2;
-            //schedule.Sum = 2;
-            //string msg = $"{schedule.From.Hour},{schedule.From.Minute},{schedule.To.Hour},{schedule.To.Minute},{schedule.Duration},{schedule.Interupt},{schedule.Sum},";
-            //MessageBox.Show(msg);
-
-            //if logged in then hide the app and run base on actor
+            //actor 0:stranger, 1: parents, 2: children
+            int actor = 0;
+            CTime schedule = ReadSchedule(SCHEDULEFILEPATH);
+            //get current time
+            var date = DateTime.Now;
+            int currentHour=date.Hour;
+            int currentMinute=date.Minute;
+            //if logged in then hide the app and run in background
             //this.ShowInTaskbar = false;
             //this.Hide();
+
+
+            if (actor == 1)
+            {
+
+            }
+            else
+            {
+                //Computer is not available at current time (either current time not fall in From-To period or in interupt time)
+                bool isInSchedule = ((currentHour > schedule.From.Hour || currentHour == schedule.From.Hour && currentMinute >= schedule.From.Minute) &&
+                    (currentHour < schedule.To.Hour || currentHour == schedule.To.Hour && currentMinute < schedule.To.Minute));
+                bool isInInteruptTime = true ; 
+                if (!isInSchedule|| isInInteruptTime)
+                {
+                    //Notify user about next available time
+                    //
+                }
+            }
 
 
             Children();
@@ -56,34 +73,33 @@ namespace ChildrenSideApp
 
         void Children()
         {
-            CTime schedule = ReadSchedule(FILEPATH);
+         
+            //create folder to save log and screenshot
+            System.IO.Directory.CreateDirectory(LOGFOLDERPATH);
 
-            //while (true)
-            //{
-            //    String filename = DateTime.Now.ToString("ddMMyyyy-hhmmss") + ".png";
-            //    int screenLeft = (int)SystemParameters.VirtualScreenLeft;
-            //    int screenTop = (int)SystemParameters.VirtualScreenTop;
-            //    int screenWidth = (int)SystemParameters.VirtualScreenWidth;
-            //    int screenHeight = (int)SystemParameters.VirtualScreenHeight;
-            //    Bitmap bitmap_Screen = new Bitmap(screenWidth, screenHeight);
-            //    Graphics g = Graphics.FromImage(bitmap_Screen);
-            //    g.CopyFromScreen(screenLeft, screenTop, 0, 0, bitmap_Screen.Size);
-            //    bitmap_Screen.Save("C:\\Users\\PC\\VNU-HCMUS\\VŨ CÔNG DUY - he dieu hanh\\thuc muc sync\\" + filename);
-            //    Thread.Sleep(5000);
-            //}
+            //create log file or open then write 
 
+            //read schedule
+            CTime schedule = ReadSchedule(SCHEDULEFILEPATH);
+            MessageBox.Show($"From: {schedule.From.Hour},{schedule.From.Minute} To: {schedule.To.Hour},{schedule.To.Minute} Duration: {schedule.Duration} Interupt: {schedule.Interupt} Sum: {schedule.Sum}");
+
+            //read log file
+            List<CTime> logList = ReadLogFile(LOGFILEPATH);
+            //foreach (CTime log in logList)
+            //    MessageBox.Show($"From: {log.From.Hour},{log.From.Minute} To: {log.To.Hour},{log.To.Minute}");
+
+            // Screenshot
+            String FILEPATH = DateTime.Now.ToString("HH-mm") + ".png";
+            ScreenShot($"{LOGFOLDERPATH}{FILEPATH}");
         }
+
+
+        //Return a CTime by reading from FILEPATH passed in
         CTime ReadSchedule(string FILEPATH)
         {
             //set duration and sum to max, interup to 0 at first
             CTime schedule = new CTime(0, 0, 0, 0, 9999999, 0, 999999999);
 
-            //Read schedule file
-            //File.WriteAllText(FILEPATH, "");
-            //StreamWriter sw = new StreamWriter(FILEPATH);
-            //sw.WriteLine("Hello World!!1234");
-            //sw.WriteLine("From the StreamWriter class");
-            //sw.Close();
             StreamReader sr = new StreamReader(FILEPATH);
             string line = sr.ReadLine();
             string[] arr = line.Split(' ');
@@ -127,8 +143,47 @@ namespace ChildrenSideApp
                 }
             }
             sr.Close();
-            MessageBox.Show($"From: {schedule.From.Hour},{schedule.From.Minute} To: {schedule.To.Hour},{schedule.To.Minute} Duration: {schedule.Duration} Interupt: {schedule.Interupt} Sum: {schedule.Sum}");
             return schedule;
+        }
+        //Return a list of CTime by reading from FILEPATH passed in
+        List<CTime> ReadLogFile(string FILEPATH)
+        {
+            List<CTime> logList = new List<CTime>();
+            StreamReader sr = new StreamReader(FILEPATH);
+            string line,hourStr,minuteStr;
+            while ((line = sr.ReadLine()) != null)
+            {
+                //set duration and sum to max, interup to 0 at first
+                CTime schedule = new CTime(0, 0, 0, 0, 9999999, 0, 999999999);
+                string[] part = line.Split(' ');
+                //read start time
+                hourStr = part[0].Substring(1, 2);
+                minuteStr = part[0].Substring(4, 2);
+                schedule.From.Hour = Int32.Parse(hourStr);
+                schedule.From.Minute = Int32.Parse(minuteStr);
+
+                //read close time
+                hourStr = part[1].Substring(1, 2);
+                minuteStr = part[1].Substring(4, 2);
+                schedule.To.Hour = Int32.Parse(hourStr);
+                schedule.To.Minute = Int32.Parse(minuteStr);
+
+                logList.Add(schedule);
+            }
+            sr.Close();
+            return logList;
+        }
+        //Screen shot and save to filepath passed in (filepath should include capture time)
+        void ScreenShot(string FILEPATH)
+        {
+            int screenLeft = (int)SystemParameters.VirtualScreenLeft;
+            int screenTop = (int)SystemParameters.VirtualScreenTop;
+            int screenWidth = (int)SystemParameters.VirtualScreenWidth;
+            int screenHeight = (int)SystemParameters.VirtualScreenHeight;
+            Bitmap bitmap_Screen = new Bitmap(screenWidth, screenHeight);
+            Graphics g = Graphics.FromImage(bitmap_Screen);
+            g.CopyFromScreen(screenLeft, screenTop, 0, 0, bitmap_Screen.Size);
+            bitmap_Screen.Save(FILEPATH);
         }
     }
 }
